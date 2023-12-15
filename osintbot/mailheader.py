ea18c8ebbedd5a777c1f7df5dd4b8915f34c7b46
bitdruid@vbox <bitdruid@vbox>
2023-12-16 00:15:30 +0100

    Add mailheader.py script for parsing email headers

--- osintbot/mailheader.py
+++ osintbot/mailheader.py
@@ -1,6 +1,35 @@
+import email
+import os.path
+
 
 def request(input):
-    response = {}
-    response_data = {"work in progress": "work in progress"}
-    response["mailheader"] = response_data
-    return response
+    if os.path.isfile(input):
+        with open(input, "r") as f:
+            header = email.message_from_file(f)
+    else:
+        header = email.message_from_string(input)
+    if header:
+        header = email.parser.HeaderParser().parsestr(str(header))
+        received = header.get_all("Received")
+        return received
+
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+    import sys
+
+    if "help" in sys.argv or "-h" in sys.argv or "--help" in sys.argv:
+        print("Usage: python3 mailheader.py '<string>' or <filepath>")
+        exit()
+
+    if len(sys.argv) > 1:
+        input = sys.argv[1]
+        response = request(input)
+        if response:
+            pprint(response)
+        else:
+            print("No data available.")
+
+    else:
+        print("Usage: python3 mailheader.py '<string>' or <filepath>")
